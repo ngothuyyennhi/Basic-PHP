be8ff156c7ff371f0237c889408988cfe84fe081
ngothuyyennhi <94053850+ngothuyyennhi@users.noreply.github.com>
2023-09-18 23:32:06 +0700

    Update Login.php

--- backend/classes/Login.php
+++ backend/classes/Login.php
@@ -1,17 +1,3 @@
 <?php
-// use 
-    // class Login{
 
-    //     public static function isLoggedIn()
-    //     {
-    //         if (isset($_COOKIE['FBID']))
-    //         {
-    //             if (Database::query("SELECT `user_id` FROM token WHERE token=:token",[':token'=>sha1($_COOKIE['FBID'])]))
-    //             {
-    //                 $user_id=Database::query("SELECT `user_id` FROM token WHERE token=:token",[':token'=>sha1($_COOKIE['FBID'])])[0]["user_id"];
-    //                 return $user_id;
-    //             }
-    //         }
-    //     }
-    // }
 ?>
